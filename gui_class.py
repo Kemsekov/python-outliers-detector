d3eedcab01f2687781486589ea093bcc46cf2623
Kemsekov <pikova98@bk.ru>
2024-05-17 12:34:13 +0500

    algorithm

--- gui_class.py
+++ gui_class.py
@@ -1,5 +1,6 @@
 import os
 import tkinter as tk
+import tkinter.ttk as ttk
 from tkinter import messagebox
 from tkinter import filedialog as fd
 import numpy as np
@@ -14,6 +15,41 @@ DEPENDENT = 'blue'
 common_button_style={
     "padx":10, "pady":10, "sticky":'nsew'
 }
+
+class MainWindow:
+    def __init__(self, root : tk.Tk,model,font = None) -> None:
+        self.root = root
+        self.font = font
+        self.model=model
+        root.title("Outlier detection")
+        root.geometry("300x300")
+        open_button = tk.Button(root, text="Data", command=self.open_data, font=font)
+        open_button.grid(row=0,column=0,**common_button_style)
+
+        save_button = tk.Button(root, text="Model", command=self.open_model, font=font)
+        save_button.grid(row=1,column=0,**common_button_style)
+
+        set_data_layout = tk.Button(root, text="Outliers", command=self.open_outliers, font=font)
+        set_data_layout.grid(row=2,column=0,**common_button_style)
+        self.data_window : DataWindow = None
+        self.model_window : ModelWindow = None
+
+    def open_data(self):
+        # TODO: fix that if we reopen data window we have to redo all steps of data loading
+        if self.data_window is not None: 
+            self.data_window.data_window.destroy()
+        self.data_window = DataWindow(self.root,self.font)
+
+    def open_model(self):
+        if self.data_window is None or self.data_window.data is None:
+            messagebox.showerror("Error","Dataset is not loaded")
+            return
+
+        if self.model_window is not None: self.model_window.model_window.destroy()
+        self.model_window=ModelWindow(self.root,self.model,self.data_window.data,self.data_window.feature_usage,self.font)
+
+    def open_outliers(self):
+        pass
 
 class FeatureUsage:
     def __init__(self,columns : list[str]) -> None:
@@ -101,63 +137,57 @@ class DataLayoutWindow:
         
         dependent_button = tk.Button(layout_window, text="Dependent", command=self.set_as_dependent, bg='blue', font=font)
         dependent_button.grid(row=3, column=0, **common_button_style)
+        self.columns=feature_usage.keys()
 
-        listbox = tk.Listbox(layout_window, selectmode='multiple', font=font)
+        features_table = Table(layout_window,rows=[[k] for k in feature_usage.keys()])
+        
         # Make the listbox fill the remaining space
-        listbox.grid(row=0, column=1, rowspan=4, **common_button_style)
-
-        self.columns = list(feature_usage.keys())
-        for column in self.columns:
-            listbox.insert(tk.END, column)
+        features_table.grid(row=0, column=1, rowspan=4, **common_button_style)
         
         for i,k in enumerate(self.columns):
             v = feature_usage[k]
-            listbox.itemconfig(i, {'bg':v})
+            id = features_table.row_id_at(i)
+            features_table.tree.item(id,tags=[v])
+        
+        features_table.tree.tag_configure(NOT_USED,background=NOT_USED)
+        features_table.tree.tag_configure(INDEPENDENT,background=INDEPENDENT)
+        features_table.tree.tag_configure(DEPENDENT,background=DEPENDENT)
 
-        self.listbox = listbox
+        self.features_table = features_table
         self.feature_usage=feature_usage
 
-    def clear(self):
-        listbox=self.listbox
+    def set_selection_value(self,value):
+        features_table=self.features_table
         # Get the indices of the selected options
-        selected_indices = listbox.curselection()
+        selected_rows_ids = features_table.tree.selection()
         # Clear previous selection highlights
-        listbox.selection_clear(0, tk.END)
-        # Set the background color of selected items to blue
-        for i in selected_indices:
-            self.feature_usage[self.columns[i]]=NOT_USED
-            listbox.itemconfig(i, {'bg':NOT_USED})
-        
+        features_table.selection_clear()
+        # clear colors
+        for row_id in selected_rows_ids:
+            row = features_table.tree.item(row_id)
+
+            self.feature_usage[row['values'][0]]=value
+            features_table.tree.item(row_id,tags=[value])
+
     def all_independent(self):
-        listbox=self.listbox
+        features_table=self.features_table
         # Clear previous selection highlights
-        listbox.selection_clear(0, tk.END)
-        # Set the background color of selected items to blue
-        for i in range(listbox.size()):
-            self.feature_usage[self.columns[i]]=INDEPENDENT
-            listbox.itemconfig(i, {'bg':INDEPENDENT})
+        features_table.selection_clear()
+        # clear colors
+        for row_index in range(len(self.columns)):
+            row_id = features_table.row_id_at(row_index)
+            row = features_table.tree.item(row_id)
+            self.feature_usage[row['values'][0]]=INDEPENDENT
+            features_table.tree.item(row_id,tags=[INDEPENDENT])
         
+    def clear(self):
+        self.set_selection_value(NOT_USED)
     def set_as_dependent(self):
-        listbox=self.listbox
-        # Get the indices of the selected options
-        selected_indices = listbox.curselection()
-        # Clear previous selection highlights
-        listbox.selection_clear(0, tk.END)
-        # Set the background color of selected items to blue
-        for i in selected_indices:
-            self.feature_usage[self.columns[i]]=DEPENDENT
-            listbox.itemconfig(i, {'bg':DEPENDENT})
+        self.set_selection_value(DEPENDENT)
         
     def set_as_independent(self):
-        listbox=self.listbox
-        # Get the indices of the selected options
-        selected_indices = listbox.curselection()
-        # Clear previous selection highlights
-        listbox.selection_clear(0, tk.END)
-        # Set the background color of selected items to blue
-        for i in selected_indices:
-            self.feature_usage[self.columns[i]]=INDEPENDENT
-            listbox.itemconfig(i, {'bg':INDEPENDENT})
+        self.set_selection_value(INDEPENDENT)
+
 
 class LabelInputBox(tk.Frame):
     def __init__(self, master, label_text, entry_var,font) -> None:
@@ -167,6 +197,71 @@ class LabelInputBox(tk.Frame):
         self.entry = tk.Entry(self,textvariable=entry_var,font=font)
         self.entry.grid(row=0,column=1)
 
+class Table(tk.Frame):
+    def __init__(self,master,rows,headings=None, show_scrollbar = True,row_selection_event = None, **args) -> None:
+        """
+        A table structure for tkinter
+        master: master widget
+        rows: 2 dimensional matrix of row values
+        headings: optional. If set will add heading on top of the table
+        show_scrollbar: adds scrollbar on the right side of the table
+        row_selection_event: Fired when row is selected. Function that accepts (table,event) as parameters.
+        """
+        super().__init__(master,**args)
+
+        tree_show = "headings" if headings is not None else ""
+        columns = [i for i in range(len(rows[0]))]
+        
+        style = ttk.Style()
+        style.configure("Treeview.Heading", font=font.Font(family='Helvetica', size=30, weight='bold'))
+
+        tree = ttk.Treeview(self, show=tree_show, columns=columns)
+
+        if headings is not None:
+            for c,h in zip(columns,headings):
+                tree.heading(c,text=h)
+        tree_scroll = ttk.Scrollbar(self,orient=tk.VERTICAL,command=tree.yview)
+        tree.configure(yscroll=tree_scroll.set)
+        for row in rows:
+            tree.insert("",tk.END,values=row)
+        
+
+        if row_selection_event is not None:
+            tree.bind("<<TreeviewSelect>>", lambda event: row_selection_event(self,event))
+        
+        self.rowconfigure(0, weight=1)
+        self.rowconfigure(1, weight=1)
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+
+        tree.grid(row=0,column=0)
+        if show_scrollbar:
+            tree_scroll.grid(row=0, column=1, sticky=tk.N + tk.S)
+        self.tree = tree
+
+        # tree.item(self.row_at(0),tags=["1"])
+    
+    def row_selection(self):
+        """returns selected rows"""
+        return [self.tree.item(selection) for selection in self.tree.selection()]
+    
+    def rows_count(self): return len(self.tree.get_children())
+
+    def row_at(self,index): 
+        """get row element under given index"""
+        return self.tree.item(self.row_id_at(index))
+
+    def row_id_at(self,index): 
+        """
+        returns row_id under given index. 
+        
+        it then can be used to change row elements like.
+
+        `table.tree.item(table.row_id_at(0),tags=["1"],values=["1","2"])`
+        
+        Example above will change row under index 0
+        """
+        return self.tree.get_children()[index]
 
 class ModelWindow:
     def __init__(self,root : tk.Tk,model, data : pd.DataFrame, feature_usage : FeatureUsage, font) -> None:
@@ -176,7 +271,8 @@ class ModelWindow:
         self.feature_usage = feature_usage
         model_window = tk.Toplevel(root)
         model_window.title("Model selection")
-        model_window.geometry("400x600")
+        model_window.geometry("800x300")
+        self.model_window=model_window
 
         model_type = tk.StringVar()
         classification_rb = tk.Radiobutton(model_window, text="Classification", variable=model_type, value="Classification", font=font)
@@ -196,7 +292,7 @@ class ModelWindow:
         repeats_frame.grid(row=1,column=1,**common_button_style)
         self.repeats=repeats
 
-        gridsearch_button = tk.Button(model_window, text="GridSearchCV", command=self.grid_search_cv)
+        gridsearch_button = tk.Button(model_window, text="GridSearchCV", command=self.grid_search_cv,font=font)
         gridsearch_button.grid(row=2, column=0, **common_button_style)
 
         cv = tk.IntVar(model_window,value=5)
@@ -204,8 +300,8 @@ class ModelWindow:
         cv_frame.grid(row=2,column=1)
         self.cv=cv
 
-        randomsearch_button = tk.Button(model_window, text="RandomSearchCV", command=self.random_search_cv)
-        randomsearch_button.grid(row=3, column=1, **common_button_style)
+        randomsearch_button = tk.Button(model_window, text="RandomSearchCV", command=self.random_search_cv,font=font)
+        randomsearch_button.grid(row=3, column=0, **common_button_style)
 
         n_iter = tk.IntVar(model_window,value=300)
         n_iter_frame = LabelInputBox(model_window,"Iterations",n_iter,font)
@@ -214,4 +310,5 @@ class ModelWindow:
     def grid_search_cv(self):
         pass
     def random_search_cv(self):
+        
         pass
